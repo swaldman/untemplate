0cf3e6cd238f50740e78c5cb1419ea3e4adcad63
Steve Waldman <swaldman@mchange.com>
2022-12-31 19:28:17 -0500

    Add some scala-cli build scripts, as I'm trying to learn that tool better.

---
+++ scala-cli-build-default.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scala-cli package --dep dev.zio::zio:2.0.5 --dep com.github.scopt::scopt:4.1.0 --dep com.mchange::literal:0.1.1-SNAPSHOT --dep com.mchange::codegenutil:0.0.1-SNAPSHOT -o untemplate ./src/main/scala/untemplate/*
+
