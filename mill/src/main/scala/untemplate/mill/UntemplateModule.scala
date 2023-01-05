7347101ed8c9f1c93891463b02e5b2c2b781c731
Steve Waldman <swaldman@mchange.com>
2023-01-05 13:31:07 -0500

    mill module kind of works.

--- mill/src/main/scala/untemplate/mill/UntemplateModule.scala
+++ mill/src/main/scala/untemplate/mill/UntemplateModule.scala
@@ -24,11 +24,10 @@ trait UntemplateModule extends ScalaModule {
 
   override def ivyDeps = T{ super.ivyDeps() ++ Agg(ivy"com.mchange::untemplate:0.0.1-SNAPSHOT") }
 
-  // from https://github.com/vic/mill-scalaxb/blob/master/scalaxb/src/ScalaxbModule.scala
+  // modified from https://github.com/vic/mill-scalaxb/blob/master/scalaxb/src/ScalaxbModule.scala
   private def filesUnder(path: Seq[PathRef], extension: String): Seq[PathRef] =
     path
       .flatMap(p => walk(p.path))
       .filter(_.toString.endsWith(extension))
       .map(PathRef(_))
-
 }
