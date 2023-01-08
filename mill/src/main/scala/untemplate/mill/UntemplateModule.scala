5dad5bdaa8397312df1167f771a339b85d6b4fed
Steve Waldman <swaldman@mchange.com>
2023-01-08 00:47:09 -0500

    Add workaround to mill module to prevent compilation of build.sc from needing to load Scala 3 code, which, currently, it can't.

--- mill/src/main/scala/untemplate/mill/UntemplateModule.scala
+++ mill/src/main/scala/untemplate/mill/UntemplateModule.scala
@@ -18,11 +18,22 @@ trait UntemplateModule extends ScalaModule {
     false
   }
 
-  def untemplateSelectCustomizer : untemplate.Customizer.Selector =
+  // we really want to return Customizer.Select directly here, but if we do,
+  // build.sc tries to resolve Scala 3 untemplate classes, and build.sc
+  // compiles without scalac option `-Ytasty-reader`, so that fails.
+  //
+  // we return Any, then cast, because even to return a Function1[I,O] would
+  // require us to refer to untemplate classes.
+  //
+  // eventually mill will more easily integrate with Scala 3, and this should
+  // be revisted.
+  
+  def untemplateSelectCustomizer : Any =
     untemplate.Customizer.NeverCustomize
 
   def untemplateGenerateScala = T {
-    Untemplate.unsafeTranspileRecursive(untemplateSource().path.toNIO, T.dest.toNIO, untemplateSelectCustomizer, untemplateFlatten())
+    val selectCustomizer = untemplateSelectCustomizer.asInstanceOf[Customizer.Selector]
+    Untemplate.unsafeTranspileRecursive(untemplateSource().path.toNIO, T.dest.toNIO, selectCustomizer, untemplateFlatten())
     PathRef(T.dest)
   }
 
@@ -32,7 +43,7 @@ trait UntemplateModule extends ScalaModule {
 
   override def ivyDeps = T{ super.ivyDeps() ++ Agg(ivy"com.mchange::untemplate:0.0.1-SNAPSHOT") }
 
-  // inspired by modified from https://github.com/vic/mill-scalaxb/blob/master/scalaxb/src/ScalaxbModule.scala
+  // inspired by https://github.com/vic/mill-scalaxb/blob/master/scalaxb/src/ScalaxbModule.scala
   private def scalaUnder( path : os.Path ) : Seq[os.Path] =
     os.walk(path).filter( os.isFile ).filter( _.last.endsWith(".scala") )
 }
