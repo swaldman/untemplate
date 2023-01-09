1b486b26d80f8ca8097abfe4235d30cf5ddc20ca
Steve Waldman <swaldman@mchange.com>
2023-01-08 22:00:18 -0500

    Implement long delimeters in regexes.

--- src/main/scala/untemplate/regex.scala
+++ src/main/scala/untemplate/regex.scala
@@ -1,22 +1,22 @@
 package untemplate
 
-private val UnanchoredHeaderDelimeterRegexString = """\((.*?)\)\[(.*)\]\~\((.*?)\)\>"""
+private val UnanchoredHeaderDelimeterRegexString = """\((.*?)\)\[(.*)\]\~+?\((.*?)\)\>"""
 private val UnanchoredHeaderDelimeterRegex = UnanchoredHeaderDelimeterRegexString.r
 private val AnchoredHeaderDelimeterRegex = ("""^"""+ UnanchoredHeaderDelimeterRegexString + """\s*$""").r
 
-private val UnanchoredTextStartDelimeterRegexString = """\((.*?)\)\>"""
+private val UnanchoredTextStartDelimeterRegexString = """\((.*?)\)[\-\>]*?\>"""
 private val UnanchoredTextStartDelimeterRegex = UnanchoredTextStartDelimeterRegexString.r
 private val AnchoredTextStartDelimiterRegex = ("""^""" + UnanchoredTextStartDelimeterRegexString + """\s*$""").r
 
-private val UnanchoredTextEndDelimeterRegexString = """\<\(\)"""
+private val UnanchoredTextEndDelimeterRegexString = """\<[\<\-]*?\(\)"""
 private val UnanchoredTextEndDelimeterRegex = UnanchoredTextEndDelimeterRegexString.r
 private val AnchoredTextEndDelimeterRegex = ("""^""" + UnanchoredTextEndDelimeterRegexString + """\s*$""").r
 
 private val EmbeddedExpressionRegex = """(?<!\\)\<\((.+?)\)\>""".r
 
-private val UnescapeHeaderDelimeterRegex = """\\(\(.*?\)\[.*?\]\~\(.*?\)\>)""".r
-private val UnescapeTextStartDelimeterRegex = """\\(\(.*?\)\>)""".r
-private val UnescapeTextEndDelimeterRegex = """\\(\<\(\))""".r
+private val UnescapeHeaderDelimeterRegex = """\\(\(.*?\)\[.*?\]\~+?\(.*?\)\>)""".r
+private val UnescapeTextStartDelimeterRegex = """\\(\(.*?\)[\-\>]*?\>)""".r
+private val UnescapeTextEndDelimeterRegex = """\\(\<[\<\-]*?\(\))""".r
 private val UnescapeEmbeddedExpressionRegex = """\\(\<\(.+?\)\>)""".r
 
 private val UnescapeRegexes =
