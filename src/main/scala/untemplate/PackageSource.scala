d152c54169511574c399d7ad88762df4fe67c8d6
Steve Waldman <swaldman@mchange.com>
2024-01-05 00:07:32 -0500

    Implement transfer/generation of side-scala files placed next to untemplates in a directory-based package hierarchy.

--- src/main/scala/untemplate/PackageSource.scala
+++ src/main/scala/untemplate/PackageSource.scala
@@ -6,6 +6,9 @@ import scala.jdk.CollectionConverters.*
 import java.nio.file.{Files,Path}
 import zio.*
 
+/**
+  *  Represents untemplate and related source files within a single, often-package-representing directory.
+  */
 object PackageSource:
 
   private def assertDirectories( paths : Iterable[Path] ) : Task[Unit] =
@@ -13,6 +16,8 @@ object PackageSource:
       require(paths.forall(p => Files.isDirectory(p)), s"""dirPath and baseDirPath must be directories! Paths: ${paths.mkString(", ")}""")
     }
 
+  // XXX: Not DRY! Added support for SideScala by just mimicking in parallel untemplate logic.
+  //      Perhaps the logic should be abstracted out in common
   private def fromDirectory( locationPackage : LocationPackage, dirPath : Path, codec : Codec ) : Task[PackageSource] =
     ZIO.attemptBlocking {
       val allFilePaths = Files.list(dirPath).toScala(Vector)
@@ -27,7 +32,18 @@ object PackageSource:
           val lines = Files.readAllLines(sourcePath, codec.charSet).asScala.to(Vector)
           UntemplateSource( sourcePath.toString, lines )
         }
-      PackageSource(locationPackage, untemplateSourceNameVec, untemplateSourceMetadata, untemplateSource)
+      val sideScalaPaths = allFilePaths.filter(path => Files.isRegularFile(path) && path.toString.endsWith(DotScalaSuffix))
+      val sideScalaSourceNameVec = sideScalaPaths.map(_.getFileName.toString)
+      val sideScalaSourceNamesToPaths = Map(sideScalaSourceNameVec.zip(sideScalaPaths): _*)
+      val sideScalaSourceMetadata: String => Task[SideScalaSource.Metadata] =
+        sideScalaSourceName => ZIO.attemptBlocking(SideScalaSource.Metadata(Some(Files.getLastModifiedTime(sideScalaSourceNamesToPaths(sideScalaSourceName)).toMillis)))
+      val sideScalaSource: String => Task[SideScalaSource] =
+        sideScalaSourceName => ZIO.attemptBlocking {
+          val sourcePath = sideScalaSourceNamesToPaths(sideScalaSourceName)
+          val lines = Files.readAllLines(sourcePath, codec.charSet).asScala.to(Vector)
+          SideScalaSource( sourcePath.toString, lines )
+        }
+      PackageSource(locationPackage, untemplateSourceNameVec, untemplateSourceMetadata, untemplateSource, sideScalaSourceNameVec, sideScalaSourceMetadata, sideScalaSource, codec)
     }
 
   def fromDirectory( dirPath : Path, baseDirPath : Option[Path] = None, codec : Codec = Codec.UTF8 ) : Task[PackageSource] =
@@ -49,5 +65,9 @@ case class PackageSource (
   locationPackage          : LocationPackage,
   untemplateSourceNames    : Vector[String],
   untemplateSourceMetadata : String => Task[UntemplateSource.Metadata],
-  untemplateSource         : String => Task[UntemplateSource]
+  untemplateSource         : String => Task[UntemplateSource],
+  sideScalaSourceNames     : Vector[String],
+  sideScalaSourceMetadata  : String => Task[SideScalaSource.Metadata],
+  sideScalaSource          : String => Task[SideScalaSource],
+  codec                    : Codec
 )
