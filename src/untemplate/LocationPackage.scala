cd35a813f6f1ca64350977abc52969179cb7ce42
Steve Waldman <swaldman@mchange.com>
2025-08-16 15:41:59 -0400

    Support prefixPackage in LocationPackage.

--- src/untemplate/LocationPackage.scala
+++ src/untemplate/LocationPackage.scala
@@ -8,16 +8,19 @@ import java.nio.file.{Files, Path}
 object LocationPackage:
   private val EmptyStringList = "" :: Nil
 
-  val empty = Nil
+  val empty   : LocationPackage = Nil
+  val default : LocationPackage = empty
 
-  def fromLocation(path : Path, baseDirPath : Option[Path] = None) : Task[LocationPackage] =
+  def fromDotty( dotty : String ) : LocationPackage = dotty.split('.').map( toIdentifier ).toList
+
+  def fromLocation(path : Path, baseDirPath : Option[Path] = None, prefixPackage : LocationPackage = LocationPackage.default ) : Task[LocationPackage] =
     ZIO.attemptBlocking {
       val pathIsDir = Files.isDirectory(path)
       val dirPath = if pathIsDir then path else path.getParent
       val rel = baseDirPath.map(_.relativize(dirPath)).getOrElse(dirPath.getFileName)
       val pieces = rel.iterator().asScala.map(_.toString).toList
-      if pieces == EmptyStringList then Nil // no pkg dir shows up as an empty string, we want an empty list
-      else pieces.map(piece => asIdentifier(piece))
+      if pieces == EmptyStringList then prefixPackage
+      else prefixPackage ::: pieces.map(piece => asIdentifier(piece))
     }
 
   extension (lp: LocationPackage)
